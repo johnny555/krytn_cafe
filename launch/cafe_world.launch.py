c4db7f69c56bedafad071e4d79af31e0d65c4d5a
John Vial <badwolf.johnnyv@gmail.com>
2023-04-10 10:17:31 +0000

    lint

--- launch/cafe_world.launch.py
+++ launch/cafe_world.launch.py
@@ -7,26 +7,27 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.actions import ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir,LaunchConfiguration
+from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
+
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='True')
     world_file_name = '/usr/share/gazebo-11/worlds/cafe.world'
     pkg_dir = get_package_share_directory('krytn_cafe')
- 
+
     os.environ["GAZEBO_MODEL_PATH"] = os.path.join(pkg_dir, 'models')
- 
+
     cafe_gazebo = ExecuteProcess(
-            cmd=['gazebo', world_file_name, '--verbose', '-s', 'libgazebo_ros_init.so', 
-            '-s', 'libgazebo_ros_factory.so'],
-            output='screen')
- 
+        cmd=['gazebo', world_file_name, '--verbose', '-s', 'libgazebo_ros_init.so',
+             '-s', 'libgazebo_ros_factory.so'],
+        output='screen')
+
     krytn = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    os.path.join(pkg_dir,'launch', 'spawn_krytn.launch.py')))
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_dir, 'launch', 'spawn_krytn.launch.py')))
 
     return LaunchDescription([
         cafe_gazebo,
-        krytn       
+        krytn
     ])
