5b5f5f12e7239310ea5fd3a1e1047da9ca02fe4e
John Vial <badwolf.johnnyv@gmail.com>
2023-04-16 13:30:23 +0000

    added robot steering as dependency

--- launch/empty_world.launch.py
+++ launch/empty_world.launch.py
@@ -7,25 +7,26 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.actions import ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir,LaunchConfiguration
+from launch.substitutions import ThisLaunchFileDir, LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
+
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='True')
     pkg_dir = get_package_share_directory('krytn_cafe')
- 
+
     os.environ["GAZEBO_MODEL_PATH"] = os.path.join(pkg_dir, 'models')
- 
+
     empty_gazebo = ExecuteProcess(
-            cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', 
-            '-s', 'libgazebo_ros_factory.so'],
-            output='screen')
- 
+        cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so',
+             '-s', 'libgazebo_ros_factory.so'],
+        output='screen')
+
     krytn = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    os.path.join(pkg_dir,'launch', 'spawn_krytn.launch.py')))
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_dir, 'launch', 'diffbot.launch.py')))
 
     return LaunchDescription([
         empty_gazebo,
-        krytn       
+        krytn
     ])
