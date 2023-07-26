74d54d72fa00035b3114421121b672700cf9c0aa
John V <badwolf.johnnyv@gmail.com>
2023-07-26 22:16:28 +0800

    slowly improving

--- launch/perception.launch.py
+++ launch/perception.launch.py
@@ -3,25 +3,43 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import PathJoinSubstitution, TextSubstitution
+
 import os
 
 
 def generate_launch_description():
+    lidar = Node(
+            name='rplidar_composition',
+            package='rplidar_ros',
+            executable='rplidar_composition',
+            output='screen',
+            parameters=[{
+                'serial_port': '/dev/ttyUSB0',
+                'serial_baudrate': 115200,  # A1 / A2
+                # 'serial_baudrate': 256000, # A3
+                'frame_id': 'laser_link',
+                'inverted': False,
+                'angle_compensate': True,
+            }],
+        )
 
-    lidar = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(
-            get_package_share_directory("rplidar_ros"),
-            'launch/rplidar.launch.py'))
+    realsense_params = DeclareLaunchArgument(
+        'config_file',
+        default_value=PathJoinSubstitution([get_package_share_directory("krytn_cafe"), 
+            'config', 'perception.yaml']),
+        description='Path to the perception parameters file.'
+    )
+
+    
+    realsense = IncludeLaunchDescription(
+       PythonLaunchDescriptionSource(
+            PathJoinSubstitution([get_package_share_directory("realsense2_camera"),
+             "launch/rs_launch.py"])),
      )
 
-    #realsense = IncludeLaunchDescription(
-    #   PythonLaunchDescriptionSource(
-    #        os.path.join(get_package_share_directory("realsense2_camera"), "launch/rs_launch.py")),
-    #    launch_arguments={
-    #        'config_file':os.path.join(get_package_share_directory("krytn_cafe","config"), 'perception.yaml')
-    ##    }
-    # )
-
     return LaunchDescription([
-        lidar
+        lidar,
+        realsense
     ])
