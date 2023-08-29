7f4927a4926eebe35349dc14a79121aef2379f60
John V <badwolf.johnnyv@gmail.com>
2023-08-29 15:10:19 +0800

    Dirty hack to get realsense to work

--- launch/control_launch.py
+++ launch/control_launch.py
@@ -67,7 +67,7 @@ def generate_launch_description():
     joy = Node(
     package='joy',
     executable='joy_node',
-    parameters=[{'dev': '/dev/input/js0', 'deadzone': 0.1, 'autorepeat_rate': 20}],
+    parameters=[{'dev': '/dev/input/js0', 'deadzone': 0.1, 'autorepeat_rate': 0.0}],
     output='screen'
     )
 
