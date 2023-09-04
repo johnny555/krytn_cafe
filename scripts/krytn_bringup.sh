511ef383cdf38e08174cec70a8a91b3ccc53bb93
John Vial <badwolf.johnnyv@gmail.com>
2023-09-04 16:52:49 +0800

    Adding SVTL to global costmap

--- scripts/krytn_bringup.sh
+++ scripts/krytn_bringup.sh
@@ -2,5 +2,6 @@
 source /opt/ros/humble/local_setup.bash
 source /home/krytn/krytn_ws/install/setup.bash
 source /home/krytn/ddsrouter_ws/install/setup.bash
-ddsrouter -c /home/krytn/krytn_ws/src/krytn_cafe/config/dds_router_krytn.yaml & 
+#ddsrouter -c /home/krytn/krytn_ws/src/krytn_cafe/config/dds_router_krytn.yaml & 
+ros2 launch krytn_cafe perception.yaml &
 ros2 launch krytn_cafe map_and_nav_launch.py  
